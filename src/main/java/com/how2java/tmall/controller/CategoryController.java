e3e1620537e2b6883e7386d969dd709d30397454
hongjinyuan <hjy1078592811@163.com>
2021-12-03 20:21:10 +0800

    仿天猫项目

--- src/main/java/com/how2java/tmall/controller/CategoryController.java
+++ src/main/java/com/how2java/tmall/controller/CategoryController.java
@@ -25,7 +25,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
- 
+ //后端页面
 @Controller
 @RequestMapping("")
 public class CategoryController {
